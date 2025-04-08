66dc5b2a8b7bfd4a604cedd220d3a18637c19fa0
yayiji <hiyayiji@gmail.com>
2025-04-09 04:24:33 +0800

    ad

--- hooks/li-update.sh
+++ hooks/li-update.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 cd ~/li || exit
+
+# Reset all local changes
+git reset --hard
+
+# Clean untracked files and folders
+git clean -fd
+
+# Pull the latest from remote
 git pull origin main
