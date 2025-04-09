f2d3d691f4142cae66f637715168b1bc30c9736c
yayiji <hiyayiji@gmail.com>
2025-04-10 05:27:00 +0800

    add

--- make-links.sh
+++ make-links.sh
@@ -1,6 +1,6 @@
 # Run this script with: sh make-links.sh
 
-BASE_DIR=~/li
+BASE=~/li
 
 makeLink() {
   local target=$1
@@ -12,21 +12,21 @@ makeLink() {
 mkdir -p ~/.config
 
 # vim and nvim
-makeLink "$BASE_DIR/vim/vim" ~/.config/vim
-makeLink "$BASE_DIR/vim/nvim" ~/.config/nvim
+makeLink "$BASE/vim/vim" ~/.config/vim
+makeLink "$BASE/vim/nvim" ~/.config/nvim
 
 #starship
-makeLink "$BASE_DIR/config/starship.toml" ~/.config/starship.toml
+makeLink "$BASE/config/starship.toml" ~/.config/starship.toml
 
 # zsh and bash
-makeLink "$BASE_DIR/zsh/.zshrc" ~/.zshrc
-makeLink "$BASE_DIR/zsh/.bashrc" ~/.bashrc
-makeLink "$BASE_DIR/zsh/.zprofile" ~/.zprofile
+makeLink "$BASE/zsh/.zshrc" ~/.zshrc
+makeLink "$BASE/zsh/.bashrc" ~/.bashrc
+makeLink "$BASE/zsh/.zprofile" ~/.zprofile
 
 # git
-makeLink "$BASE_DIR/git/.gitconfig" ~/.gitconfig
+makeLink "$BASE/git/.gitconfig" ~/.gitconfig
 
 # mystart
-makeLink "$BASE_DIR/mystarts/webhook.service" /etc/systemd/system/webhook.service
+makeLink "$BASE/mystarts/webhook.service" /etc/systemd/system/webhook.service
 
 echo "Done!"
