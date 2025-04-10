a105256c0ebdbab2b37b036e1d1e8c511847ab68
yayiji <hiyayiji@gmail.com>
2025-04-10 18:44:14 +0800

    add

---
+++ pm.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "git-update-hooks",
+      script: "webhook",
+      args: "-hooks ~/li/hooks/hooks.json -port 9000",
+      // optional: define cwd if needed
+      // cwd: '/full/path/to/your/working/directory',
+      // optional: set autorestart, logs, environment variables, etc.
+      autorestart: true,
+    },
+    {
+      name: "ytdlp",
+      script: "node",
+      args: "/root/proj/ytdlp/index.js",
+      autorestart: true,
+    },
+  ],
+};
