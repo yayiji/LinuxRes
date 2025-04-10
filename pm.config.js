e366eea02e61c10b095a460d7368834e1c710ee3
yayiji <hiyayiji@gmail.com>
2025-04-10 19:19:20 +0800

    add

--- pm.config.js
+++ pm.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: "git-update",
-      script: "/usr/bin/webhook",
-      args: "-hooks ~/li/hooks/hooks.json -port 9000 -verbose", 
-      interpreter: "none", // No interpreter, as it’s a binary
+      script: "/root/li/hooks/start-hooks.sh",
+      interpreter: "/usr/bin/bash", // No interpreter, as it’s a binary
       // optional: define cwd if needed
       // cwd: '/full/path/to/your/working/directory',
       // optional: set autorestart, logs, environment variables, etc.
