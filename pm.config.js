d2b7307bec948da18e477c0b50a240f748e6411e
yayiji <hiyayiji@gmail.com>
2025-04-10 19:12:00 +0800

    add

--- pm.config.js
+++ pm.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: "git-update",
       script: "/usr/bin/webhook",
       args: "-hooks ~/li/hooks/hooks.json -port 9000 -verbose", 
+      interpreter: "none", // No interpreter, as it’s a binary
       // optional: define cwd if needed
       // cwd: '/full/path/to/your/working/directory',
       // optional: set autorestart, logs, environment variables, etc.
@@ -11,8 +12,8 @@ module.exports = {
     },
     {
       name: "ytdlp",
-      script: "node",
-      args: "/root/proj/ytdlp/index.js",
+      script: "/root/proj/ytdlp/index.js",
+      interpreter: "node",
       autorestart: true,
     },
   ],
